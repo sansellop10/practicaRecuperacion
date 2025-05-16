3703d6b100000036554a84e7e658779e9ddf7037
Santi <sansellop@alu.edu.gva.es>
2025-05-16 13:17:36 +0200

    Implemento menu

--- App.java
+++ App.java
@@ -7,16 +7,33 @@ public class App{
             mostrarMenu();
             int opcion = in.nextInt();
             in.nextLine();
-            gestionarOpciones();
+            gestionarOpciones(opcion);
             if (opcion == 5) {
                 bandera = false;
             }
         }
     }
     public static void mostrarMenu(){
-        System.out.println("\nMenu por implementar");
+        System.out.println("\nMenu de calculadora");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("\nIntroduce opcion");
+        System.out.println(" >");
     }
-    public static void gestionarOpciones(){
-        System.out.println("Por implementar");
+    public static void gestionarOpciones(int opcion){
+        switch(opcion){
+            case 1:
+                System.out.println("Falta por implementar la suma");
+            break;
+            case 2:
+                System.out.println("Falta por implementar la resta");
+            break;
+            case 5:
+                System.out.println("Saliendo...");
+            break;
+            default:
+                System.out.println("Error");
+            break;
+        }
     }
 }
